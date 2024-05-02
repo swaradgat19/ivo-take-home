2719e4e06a15051faccbe88e89b9794be24484d9
swaradgat19 <swarad2000@gmail.com>
2024-05-02 17:41:57 -0400

    feat - add types and read json

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface ItemProps {
+    color?: string;
+    type?: string;
+    title?: string;
+    children?: any;
+    id?: string;
+    value?: string;
+    bold?: boolean;
+    underline?: boolean;
+    variableType?: string;
+    text?: string;
+}
